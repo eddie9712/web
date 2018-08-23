c150e40eeaab747b46c5aa94e633b35792fc72e4
eddie <qw1562435@gmail.com>
2018-08-24 01:35:30 +0800

    test

--- crawler/server.js
+++ crawler/server.js
@@ -1,17 +1,41 @@
-const request = require('request')
 const cheerio=require('cheerio')
 const iconv=require('iconv-lite')
 const buffer=require('buffer').Buffer;
-const url = 'http://course-query.acad.ncku.edu.tw/qry/qry001.php?dept_no=C2'
-request(url, (err, res, body) => {
-  
- if(err)
+const request = require('request').defaults({jar: true});
+const url = 'http://course-query.acad.ncku.edu.tw/qry/qry001.php?dept_no=A1';
+const cookie="lang=CHT; PHPSESSID=E940511447330a98e095e1d3c5c7dceb0da166ffd" 
+function publishpost(){
+ var options={
+ url:'https://course.ncku.edu.tw/course/second2.php',
+ method:'POST',
+ headers:{
+  'cookie':cookie, 
+ },
+ form:{
+ 'STEP':'0',
+ 'depNo':'A1',
+ 'seqNo':'534',
+  },
+};   
+  request(options,(err,res,body)=>
+  {
+    console.log("submit");
+     })
+} //to create the request
+setInterval(
+  ()=>{
+  request(url, (err, res, body) => { 
+ if(err)  1 
    throw error;
    const $ = cheerio.load(body)
-  var test=$('TD:contains("C227400")').text();  
-     console.log(test);
+   var test=$('TD:contains("A122950")').next().next().next().next().next().next().next().next().next().next().next().next().text();  
+if(test!="額滿")
+  publishpost();
+    });
+  },5000
 
-})
+  );// each detection for 5secs 
+
                     
 
 
